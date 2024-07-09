4e381be100c4f682078a267cd53057696f209be8
Priyanshu Mehta <pmehta305@gatech.edu>
2024-07-09 18:31:27 -0400

    Test

--- server/server.py
+++ server/server.py
@@ -1,10 +1,11 @@
+import json
 import logging
 import os
-import pdfplumber
-import json
-import tempfile
 import subprocess
-from flask import Flask, request, jsonify
+import tempfile
+
+import pdfplumber
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 from openai import OpenAI
 
@@ -14,14 +15,15 @@ CORS(app)
 # Configure logging
 logging.basicConfig(level=logging.INFO)
 
+
 @app.route("/api/upload", methods=["POST"])
 def upload_file():
-    if 'file' not in request.files:
-        return jsonify({'error': 'No file part'})
+    if "file" not in request.files:
+        return jsonify({"error": "No file part"})
 
-    file = request.files['file']
-    if file.filename == '':
-        return jsonify({'error': 'No selected file'})
+    file = request.files["file"]
+    if file.filename == "":
+        return jsonify({"error": "No selected file"})
 
     # Save the uploaded file to a temporary location
     temp_file_path = os.path.join(tempfile.gettempdir(), file.filename)
@@ -33,26 +35,25 @@ def upload_file():
         # Outputs json to '../aclready/src/components/Sidebar/sample_output' + '.json'
         # This is .gitignored if you push to github. We should changed
         subprocess.run(
-            ['python', 'process_file.py', temp_file_path],
+            ["python", "process_file.py", temp_file_path],
             check=True,
             capture_output=True,
-            text=True
+            text=True,
         )
         # temporary output (hardcoded for now)
-        output_file_path = '../aclready/src/components/Sidebar/sample_output' + '.json'
+        output_file_path = "../aclready/src/components/Sidebar/sample_output" + ".json"
         logging.info(f"Output JSON file saved to {output_file_path}")
 
-        with open(output_file_path, 'r') as json_file:
+        with open(output_file_path, "r") as json_file:
             data = json.load(json_file)
-        
+
         return jsonify(data), 200
     except subprocess.CalledProcessError as e:
         logging.error(f"Error processing file: {e.stderr}")
     except Exception as e:
         logging.error(f"Unexpected error: {e}")
 
-    return jsonify({'message': 'File processed successfully'}), 200
-
+    return jsonify({"message": "File processed successfully"}), 200
 
 
 if __name__ == "__main__":
